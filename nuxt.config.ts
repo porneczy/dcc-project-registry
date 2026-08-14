6938643fa5ec822c27b19bc5669664abe7dc9b6d
porneczy <porneczy@gmail.com>
2026-08-14 14:30:01 +0200

    chore: configure project foundation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+
+  devtools: {
+    enabled: true,
+  },
+
+  css: ['~/assets/css/tailwind.css'],
+
+  modules: [
+    '@pinia/nuxt',
+    'shadcn-nuxt',
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui',
+  },
 })
